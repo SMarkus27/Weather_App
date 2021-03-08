5faa045e4767af8d96957e42da5ae7b23b1dc05d
SMarkus27 <pereira.markus.27@hotmail.com>
2021-03-08 16:59:01 -0300

    create the city models

--- weather_app/weather/models.py
+++ weather_app/weather/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class City(models.Model):
+
+    city = models.CharField(max_length=100, primary_key=True)
+
+    def __str__(self):
+       return self.city
+
+    class Meta:
+        verbose_name_plural = 'Cities'
